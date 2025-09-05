f1b88e7b2902fafb02088b3c76411d575bead3e9
santi1-12 <santiago_yepesca@soy.sena.edu.co>
2025-09-04 22:10:39 -0500

    agrego y mejoro el backup.js y agrego el gignore para que no se suban tantos datos que no necesitamos y en el js agrego la funcion para ejecutar el backend.js

--- backend/src/config/backup.js
+++ backend/src/config/backup.js
@@ -1,20 +1,29 @@
 /* eslint-disable no-unused-vars */
 const { exec } = require('child_process');
-require('dotenv').config(); process.loadEnvFile('./.env')
+require('dotenv').config();
 
 exports.backupDatabase = async () => {
-    const dbName = 'PetMarket';
     const outputPath = './backup';
 
-    const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${process.env.DB_NAME}?retryWrites=true&w=majority`;
-    const command = `"C:\\Program Files\\MongoDB\\Tools\\100\\bin\\mongodump.exe" --uri "${mongoUri}" --out ${outputPath} --gzip`;
+    // Usa la variable de entorno MONGO_DB_NAME
+    const dbName = process.env.MONGO_DB_NAME;
 
+    const mongoUri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@${process.env.DB_HOST}/${dbName}?retryWrites=true&w=majority`;
+    const command = `"C:\\Users\\yepes\\OneDrive\\Desktop\\mongodb-database-tools-windows-x86_64-100.13.0\\mongodb-database-tools-windows-x86_64-100.13.0\\bin\\mongodump.exe" --uri "${mongoUri}" --out ${outputPath} --gzip`;
 
-    await exec(command, (error, stdout, stderr) => {
+    exec(command, (error, stdout, stderr) => {
         if (error) {
-            console.error(`Error en el respaldo: ${error.message}`);
+            console.error(`❌ Error en el respaldo: ${error.message}`);
             return;
         }
-        console.log(`Respaldo completado con total éxito ${stdout}`);
+        if (stderr) {
+            console.warn(`⚠️ Advertencia: ${stderr}`);
+        }
+        console.log(`✅ Respaldo completado con total éxito\n${stdout}`);
     });
 };
+
+//  Ejecutar directamente si este archivo se llama con `node src/config/backup.js`
+if (require.main === module) {
+    exports.backupDatabase();
+}
